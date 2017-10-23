8fbe754a31fd5b2c38bac40ab0adfb54cf8dc2ba
RomSem <romasemenyshyn@gmail.com>
2017-10-23 13:48:27 +0300

    Added working requirejs

---
+++ ajax/js/config.js
@@ -0,0 +1,11 @@
+requirejs.config({
+    path:{
+
+        'jquery' :'libs/jquery.min',
+        'jquery-ui' :'libs/jquery.ui',
+        'table.jQuery' : 'js/table.jQuery.js'
+    }
+
+
+});
+
---
+++ factorial.js
@@ -0,0 +1,9 @@
+function Factorial(n) {
+    for(var i = 0; i<= n; i++){
+        n = n-1;
+
+    }
+    return n;
+}
+
+console.log(Factorial(5));
